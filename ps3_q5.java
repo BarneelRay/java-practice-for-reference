952db17280a3adc6b2faa14c6644e45196f5d569
Barneel Ray <98656468+BarneelRay@users.noreply.github.com>
2022-07-20 22:28:44 +0530

    Create ps3_q5.java

---
+++ ps3_q5.java
@@ -0,0 +1,12 @@
+package com.barneel;
+
+import java.util.Scanner;
+
+//format a code using escape sequence characters.
+public class ps3_q5
+{
+    public static void main(String[] args)
+    {
+        System.out.println("this is a good one, \n\t joey!!");
+    }
+}
