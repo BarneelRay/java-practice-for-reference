b329c10ffe032592e003e40294f67762a3d0cdbb
Barneel Ray <98656468+BarneelRay@users.noreply.github.com>
2022-07-20 14:35:23 +0530

    Create ps1_q3.java

---
+++ ps1_q3.java
@@ -0,0 +1,16 @@
+package com.barneel;
+
+import java.util.Scanner;
+
+//Greeting the user.
+
+public class ps1_q3
+{
+    public static void main(String[] args)
+    {
+        String name;
+        Scanner sc= new Scanner(System.in);
+        name=sc.next();
+        System.out.println("Hello my friend,"+name);
+    }
+}
