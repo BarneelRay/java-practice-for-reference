94052a15b806e6361268b0a12e2b27f0e812eef6
Barneel Ray <98656468+BarneelRay@users.noreply.github.com>
2022-07-21 23:53:00 +0530

    Create ps5_q1.java

---
+++ ps5_q1.java
@@ -0,0 +1,17 @@
+package com.barneel.ps5;
+
+//Pattering printing program.
+public class ps5_q1
+{
+    public static void main(String[] args)
+    {
+        for(int i=4 ;i>0;i--)
+        {
+            for(int j =0; j<i ;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
